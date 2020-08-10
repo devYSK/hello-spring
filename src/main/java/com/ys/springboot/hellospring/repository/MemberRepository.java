76449679e6d2f43adcb2615f78f7a1fca3bb22a8
devYSK <kim206gh@naver.com>
2020-08-10 19:32:43 +0900

    회원 리포지토리 테스트 케이스 작성

---
+++ src/main/java/com/ys/springboot/hellospring/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.ys.springboot.hellospring.repository;
+
+import com.ys.springboot.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    Optional<Member> findById(Long id);
+
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+}
